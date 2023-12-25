334416abc9c91a291ec0609646f35bbb49f79d6b
AgentDolo <154779892+AgentDolo@users.noreply.github.com>
2023-12-25 02:43:05 -0500

    Update script.js
    changed path to s3 bucket
    
    https://wzmaprotationimages.s3.amazonaws.com/Ashika+Island.jpg

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Array of maps with their names and image locations
     const maps = [
-        { name: "ASHIKA ISLAND", imageUrl: "/var/www/html/images/Ashika Island.jpg" },
-        { name: "VONDEL", imageUrl: "/var/www/html/images/Vondel.jpg" }
+        { name: "ASHIKA ISLAND", imageUrl: "https://wzmaprotationimages.s3.amazonaws.com/Ashika+Island.jpg" },
+        { name: "VONDEL", imageUrl: "https://wzmaprotationimages.s3.amazonaws.com/Vondel.jpg" }
         // Add more maps as needed
     ];
 
