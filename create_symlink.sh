7f8cbe434a89e761b95085b30033320f8293acbe
AgentDolo <154779892+AgentDolo@users.noreply.github.com>
2023-12-29 00:22:36 -0500

    Update create_symlink.sh

--- create_symlink.sh
+++ create_symlink.sh
@@ -9,9 +9,9 @@ while true; do
 
     # Check the content and create/override the symbolic link accordingly
     if [ "$map_name" == "Ashika" ]; then
-        ln -sf https://wzmaprotationimages.s3.amazonaws.com/Ashika.webp "$symlink"
+        ln -sf images/Ashika.webp "$symlink"
     elif [ "$map_name" == "Vondel" ]; then
-        ln -sf https://wzmaprotationimages.s3.amazonaws.com/Vondel.webp "$symlink"
+        ln -sf images/Vondel.webp "$symlink"
     else
         echo "Unsupported map name: $map_name"
     fi
