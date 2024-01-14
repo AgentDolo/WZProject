ac9da08f1c5a760b178623ea869f6856ab2078d1
AgentDolo <154779892+AgentDolo@users.noreply.github.com>
2024-01-14 10:46:07 -0500

    Base V2 create_symlink4.sh

---
+++ create_symlink4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Target symbolic link
+symlink="quadsmap.jpg"
+
+while true; do
+    # Read the content of triosResurgenceMap.txt
+    map_name=$(<quadsResurgenceMap.txt)
+
+    # Check the content and create/override the symbolic link accordingly
+    if [ "$map_name" == "Zaravan Suburbs" ]; then
+        ln -sf images/Zaravan.webp "$symlink"
+    elif [ "$map_name" == "Popov Power" ]; then
+        ln -sf images/Popov.webp "$symlink"
+    elif [ "$map_name" == "Orlov Military Base" ]; then
+        ln -sf images/Military.webp "$symlink"
+    else
+        echo "Unsupported map name: $map_name"
+    fi
+
+    # Wait for 1 seconds before checking again
+    sleep 1
+done
