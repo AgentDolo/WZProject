2b1d2b4722d190193ff2e3817c2e8add713329c8
AgentDolo <154779892+AgentDolo@users.noreply.github.com>
2024-01-14 10:47:32 -0500

    Base V2 create_symlink.sh

--- create_symlink.sh
+++ create_symlink.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Target symbolic link
-symlink="quadsmap.jpg"
+symlink="solosmap.jpg"
 
 while true; do
     # Read the content of solosResurgenceMap.txt
-    map_name=$(<quadsResurgenceMap.txt)
+    map_name=$(<solosResurgenceMap.txt)
 
     # Check the content and create/override the symbolic link accordingly
     if [ "$map_name" == "Vondel" ]; then
         ln -sf images/Vondel.webp "$symlink"
-    elif [ "$map_name" == "Ashika" ]; then
+    elif [ "$map_name" == "Ashika Island" ]; then
         ln -sf images/Ashika.webp "$symlink"
     else
         echo "Unsupported map name: $map_name"
